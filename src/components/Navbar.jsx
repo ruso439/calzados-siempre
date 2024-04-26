70f8c563509289b9a46dd350eaf0abb902b4779b
ruso439 <ruso439@gmail.com>
2024-04-26 09:32:23 -0300

    correcion de JSON

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <nav className="bg-gray-800 p-8" >
             <div className="container mx-auto">
                 <div className="flex items-center justify-between">
-                    <div className="text-white"><img src="src\firebase\img\logos\logo1.svg" className="w-20 h-auto" alt="Logo" /></div>
+                    <div className="text-white"><img src="https://firebasestorage.googleapis.com/v0/b/calzados-siempre.appspot.com/o/logo%2Flogo1.svg?alt=media&token=06ab8cfc-e556-40d8-90aa-df86624e11dc" className="w-40 h-auto" alt="Logo" /></div>
                     <ul className="flex space-x-4">
                         <Categories />
                         <CartWidget />
