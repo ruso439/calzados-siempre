8ebe24dcaa7ffc6d4b40b83344b7e64691c9f2c5
anthony <anthony.rosman_ext@atrapalo.com>
2024-04-26 09:33:54 -0400

    fix: use counter variables

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
 
-export const useCounter = (minValue = 0, maxValue = 1, step = 1) => {
-    const [count, setCount] = useState(minValue)
+export const useCounter = ({ minValue = 0, maxValue = 1, step = 1 } = {}) => {
+  const [count, setCount] = useState(() => minValue);
 
-    const increment = () => {
-        if(count + setp <= maxValue)
-            setCount(count + step)
-    }
+  const increment = () => {
+    if (count + step <= maxValue) setCount(count + step);
+  };
 
-    const decrement = () => {
-        if(count - setp >= minValue)
-            setCount(count + step)
-    }
+  const decrement = () => {
+    if (count - step >= minValue) setCount(count - step);
+  };
 
-    const reset = () => {
-        setCount(minValue)
-    }
+  const reset = () => {
+    if (count !== minValue) setCount(minValue);
+  };
 
-    return { count, increment, decrement, reset}
-}
+  return { count, increment, decrement, reset };
+};
